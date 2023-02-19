f3b16d21c04f9f6c82d0f74604ef064694b0534d
Kalmi <linkee12@gmail.com>
2023-02-19 11:38:33 +0100

    proba

--- teszt.js
+++ teszt.js
@@ -1,2 +1,12 @@
-let data = document.getElementById("inputlist")
-console.log(data)
+
+let input = document.getElementById("inputlist")
+input.addEventListener("keypress", e)
+function e(){
+    let data = input.value 
+   
+   
+   
+    localStorage.setItem("lista", data) ;
+    document.getElementById("todo").innerHTML = localStorage.getItem("lista");
+
+}
